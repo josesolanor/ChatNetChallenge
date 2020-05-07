595ad4370470f50c55a1c94e6a8ffaafe94f32c1
Jose Solano R <josesolano@outlook.com>
2020-05-07 18:15:18 -0400

    Creation of the infrastructure layer

---
+++ ChatNetChallenge/Chat.Infrastructure/Data/ApplicationDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Chat.Infrastructure.Data
+{
+    class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ ChatNetChallenge/Chat.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Chat.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Chat.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDBContext>(options => options.UseSqlite("Filename=ChatDatabase.db"));
+
+            return services;
+        }
+    }
+}
