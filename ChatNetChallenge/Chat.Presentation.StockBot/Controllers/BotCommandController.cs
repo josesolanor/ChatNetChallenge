26704dfe80208dc48353cff52dbd5b9cf7bb264d
Jose Solano R <josesolano@outlook.com>
2020-05-08 18:21:06 -0400

    Creating StockBot API

---
+++ ChatNetChallenge/Chat.Presentation.StockBot/Controllers/BotCommandController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chat.Presentation.StockBot.Controllers
+{
+    public class BotCommandController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] string text)
+        {
+            return Ok(text);
+        }
+    }
+}
